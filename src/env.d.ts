4942201bfd79be8165c2f5d486727874cdc69889
Tim Vick <t.vick@yahoo.com>
2023-09-13 21:51:58 -0700

    feat: creates new game service

--- src/env.d.ts
+++ src/env.d.ts
@@ -4,13 +4,14 @@ import type { AdvancedRuntime } from "@astrojs/cloudflare";
 import type { User } from "@clerk/backend";
 import type { Client } from "@libsql/client";
 import { TeamsService } from "./services/database/teams-service";
-import { GamesService } from "./services/database/database-service";
+import { GamesService } from "./services/games.service";
 
-declare namespace App {
+declare namespace Astro {
 	interface Locals extends AdvancedRuntime {
 		user: User;
 		db: Client;
 		environment: string;
 		teamsService: TeamsService;
+		gamesService: GamesService;
 	}
 }
--- src/types.ts
+++ src/types.ts
@@ -38,7 +38,7 @@ export type DBGame = {
 	away_team_id: number;
 	home_team_score: number;
 	away_team_score: number;
-	game_time: number;
+	game_time: Date;
 	spread: number;
 	created_at: number;
 	updated_at: number;
